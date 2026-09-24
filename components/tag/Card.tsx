c0a914d32712193746671fab717231d963a58cf7
Palentum <79781425+Palentum@users.noreply.github.com>
2026-09-24 13:07:15 +0800

    style(tag-card): remove redundant text from count display

--- components/tag/Card.tsx
+++ components/tag/Card.tsx
@@ -24,7 +24,7 @@ export const TagCard = ({ tag }: Props) => {
             {tag.name}
           </h2>
           <Chip size="sm" variant="flat">
-            {tag.count} 个 Galgame
+            {tag.count}
           </Chip>
         </div>
         {aliases.length > 0 && (
